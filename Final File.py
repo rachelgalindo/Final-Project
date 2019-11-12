74114bdc0bda3088a4de03e0a752518f1eada8ed
Rachel Galindo <rachelgalindo18@yahoo.com>
2019-11-11 23:21:23 -0500

    added pseudocode

--- Final File.py
+++ Final File.py
@@ -1,1 +1,46 @@
-#Final project File 
+#Final project File
+
+#Create a homepage here
+#Title of the quiz
+#Description of the quiz
+#button to begin
+
+#Begin quiz questions
+#need to store the input from the user
+#every option needs to be a button
+#transform input into integers to add to different categories
+
+#Quiz aesthetics
+#every question should have four options to answer
+#each option should come with a picture to click on
+#each option should also have a text box under it for a description
+#create a transition from one question to the next?
+
+#Create a page to see quiz results
+
+#Output activity to the user:
+#activity should come with its own background/picture
+#add in a sound effect?
+#add in danger level? 
+#add an option to take the quiz again
+#add an option to exit out
+
+import tkinter as tk
+    
+x = 0
+def definex(x):
+    print(x)
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Hello",
+                   command=definex(x +1))
+slogan.pack(side=tk.LEFT)
+                   
+root.mainloop()
+
